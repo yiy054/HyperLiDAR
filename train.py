2f6dcb2eea3a2f7e1cd104fb3f4630622ed9c882
Ivannia Gomez <82829719+DarthIV02@users.noreply.github.com>
2024-09-26 01:05:13 -0700

    Update train.py

--- train.py
+++ train.py
@@ -154,7 +154,8 @@ for epoch in range(0, cfg.trainer.epoch):
 
     mIoU, per_class_iou = compute_mIoU_torch(preds_total, labels, cfg.n_classes) # Change when more datasets
     print(f"Val mIoU in epoch {epoch}: ", mIoU, per_class_iou)
-    print(labels)
+    print(labels+1)
+    print(torch.min(labels+1))
     print(torch.bincount((labels+1).to(torch.int)))
     
 
