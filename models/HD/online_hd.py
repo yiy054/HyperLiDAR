2fb4fcc0bd4862b02bba42eb865bc19dcea4ab93
Ivannia Gomez <82829719+DarthIV02@users.noreply.github.com>
2024-09-26 16:08:24 -0700

    Try Projection

--- models/HD/online_hd.py
+++ models/HD/online_hd.py
@@ -34,7 +34,7 @@ class OnlineHD(Classifier):
 
     """
 
-    encoder: Density
+    encoder: Projection
     model: Centroid
 
     def __init__(
@@ -63,10 +63,10 @@ class OnlineHD(Classifier):
             self.encoders = {}
             for mark in self.cfg.bundle:
                 features = 2**(((mark + 1)//3)+6)
-                self.encoders[mark] = Sinusoid(features, n_dimensions, device=device, dtype=dtype)
+                self.encoders[mark] = Projection(features, n_dimensions, device=device, dtype=dtype)
                 self.features[mark] = features
         else:
-            self.encoder = Sinusoid(n_features, n_dimensions, device=device, dtype=dtype)
+            self.encoder = Projection(n_features, n_dimensions, device=device, dtype=dtype)
         self.model = Centroid(n_dimensions, n_classes, device=device, dtype=dtype)
 
     def fit(self, samples, labels):
