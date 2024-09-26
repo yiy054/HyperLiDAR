0c14043b79b742ce41907451e4fdd3e370f47a2f
Ivannia Gomez <82829719+DarthIV02@users.noreply.github.com>
2024-09-25 20:57:29 -0700

    Update train.py

--- train.py
+++ train.py
@@ -87,6 +87,8 @@ for epoch in range(0, cfg.trainer.epoch):
                                         num_workers=cfg.threads)
 
     t_val = tqdm(val_loader, ncols=100, desc="Val Epoch {}".format(epoch), disable=False)
+    print("Total Length:", ds_val.__len__)
+    pred_complete = np.zeros(())
     for data_val in t_val:
         pts = data_val['pts']#.to(device)
         features = data_val['features']#.to(device)
