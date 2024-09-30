86af93c50cbd99da4d2a690ebb75da0951a111de
Ivannia Gomez <82829719+DarthIV02@users.noreply.github.com>
2024-09-30 11:10:19 -0700

    Add nuscenes

---
+++ auxiliary/process_data/nuscenes/prepare_data.py
@@ -0,0 +1,78 @@
+import argparse
+import os
+import numpy as np
+from sklearn.decomposition import PCA
+from nuscenes.nuscenes import NuScenes
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--rootdir", type=str, required=True)
+parser.add_argument("--destdir", type=str, required=True)
+parser.add_argument("--test", action="store_true")
+args = parser.parse_args()
+
+nusc = NuScenes(version='v1.0-mini', dataroot=args.rootdir, verbose=True)
+
+# create the directory
+train_filenames = []
+test_filenames = []
+
+if args.test:
+    filenames = test_filenames
+    save_dir = os.path.join(args.destdir,"test_pointclouds")
+else:
+    filenames = train_filenames
+    save_dir = os.path.join(args.destdir,"train_pointclouds")
+os.makedirs(save_dir, exist_ok=True)
+
+
+for sc in nusc.scene:
+    file = []
+    labels = []
+    sample = nusc.get('sample',sc['first_sample_token'])
+    sample_data = sample['data']['LIDAR_TOP']
+    print(sample_data.shape)
+    x = 0 
+    y = 0
+    z = 0
+    reflectance = 0
+    if not args.test:
+        label = 0
+    
+    if args.test:
+        pts = np.concatenate([
+                np.expand_dims(x,1),
+                np.expand_dims(y,1),
+                np.expand_dims(z,1),
+                np.expand_dims(reflectance,1),
+                ], axis=1).astype(np.float32)
+
+        np.save(os.path.join(save_dir, os.path.splitext(filename)[0]), pts)
+        
+    else:
+        pts = np.concatenate([
+                np.expand_dims(x,1),
+                np.expand_dims(y,1),
+                np.expand_dims(z,1),
+                np.expand_dims(reflectance,1),
+                np.expand_dims(label,1),
+                ], axis=1).astype(np.float32)
+
+        pca = PCA(n_components=1)
+        pca.fit(pts[::10,:2])
+        pts_new = pca.transform(pts[:,:2])
+        hist, edges = np.histogram(pts_new, pts_new.shape[0]// 2500000)
+
+        count = 0
+
+        for i in range(1,edges.shape[0]):
+            mask = np.logical_and(pts_new<=edges[i], pts_new>edges[i-1])[:,0]
+            np.save(os.path.join(save_dir, os.path.splitext(filename)[0]+f"_{count}"), pts[mask])
+            count+=1
+
+
+        hist, edges = np.histogram(pts_new, pts_new.shape[0]// 2500000 -2, range=[(edges[1]+edges[0])//2,(edges[-1]+edges[-2])//2])
+
+        for i in range(1,edges.shape[0]):
+            mask = np.logical_and(pts_new<=edges[i], pts_new>edges[i-1])[:,0]
+            np.save(os.path.join(save_dir, os.path.splitext(filename)[0]+f"_{count}"), pts[mask])
+            count+=1
