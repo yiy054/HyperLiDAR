415a2abe8a783d384f16a34c0379650ba98aab16
Ivannia Gomez <82829719+DarthIV02@users.noreply.github.com>
2024-11-25 14:59:59 -0800

    Update main_features.py

--- main_features.py
+++ main_features.py
@@ -128,7 +128,7 @@ for i in range(num_samples):
 
     # HD training
     samples_hv = encode(first_sample)
-    model.add_online(samples_hv, first_label, lr=0.0005)
+    model.add_online(samples_hv, first_label, lr=0.00001)
 
     # HD prediction
     #pred_hd = model(samples_hv, dot=True).argmax(1).data
