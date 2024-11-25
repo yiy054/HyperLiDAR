2bcabab42d13ea9fdaa50f04a24af6c548214be3
Ivannia Gomez <82829719+DarthIV02@users.noreply.github.com>
2024-11-25 12:20:13 -0800

    Update main_features.py

--- main_features.py
+++ main_features.py
@@ -166,7 +166,7 @@ for i in range(num_samples):
     print('pred_hd', pred_hd)
     print('label', first_label)
     accuracy = miou(pred_hd, first_label)
-    cm = confusion_matrix(pred_hd, first_label, labels=CLASS_NAME)
+    cm = confusion_matrix(pred_hd, first_label, labels=torch.Tensor(range(0,15)))
     print("Confusion matrix \n")
     print(cm)
     avg_acc = torch.mean(accuracy)
