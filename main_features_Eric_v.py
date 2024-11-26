161b8ef1db42a35f4b9164749d8ecf829fa3986f
Ivannia Gomez <82829719+DarthIV02@users.noreply.github.com>
2024-11-25 22:38:45 -0800

    Update main_features_Eric_v.py

--- main_features_Eric_v.py
+++ main_features_Eric_v.py
@@ -57,16 +57,16 @@ class HD_Model:
 
             first_sample = self.normalize(first_sample) # Z1 score seems to work
 
-            for vox in range(len(first_sample)):
+            #for vox in range(len(first_sample)):
                 
-                label = first_label[vox]
-                if vox % 5000 == 0:
-                    print(f"Sample {i}: Voxel {vox}")
+            #    label = first_label[vox]
+            #    if vox % 5000 == 0:
+            #        print(f"Sample {i}: Voxel {vox}")
                 
-                # HD training
-                samples_hv = self.encode(first_sample[vox])
-                samples_hv = samples_hv.reshape((1,samples_hv.shape[0]))
-                self.model.add(samples_hv, label)
+            # HD training
+            samples_hv = self.encode(first_sample)
+            #samples_hv = samples_hv.reshape((1,samples_hv.shape[0]))
+            self.model.add(samples_hv, first_label)
 
     def retrain(self, features, labels, num_voxels):
         for e in tqdm(range(10), desc="Epoch"):
