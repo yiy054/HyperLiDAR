594ea61995d2941cfdd2524a9d29ecd5c5516938
Ivannia Gomez <82829719+DarthIV02@users.noreply.github.com>
2024-11-30 00:15:20 -0800

    Update pc_dataset.py

--- datasets/pc_dataset.py
+++ datasets/pc_dataset.py
@@ -54,7 +54,7 @@ class PCDataset(Dataset):
         self.downsample = tr.Voxelize(
             dims=(0, 1, 2),
             voxel_size=voxel_size,
-            random=(self.phase == "train" or self.phase == "trainval"),
+            random=False,#(self.phase == "train" or self.phase == "trainval"),
         )
 
         # Field of view
@@ -143,7 +143,7 @@ class PCDataset(Dataset):
 
         # Voxelization
         pc, labels = self.downsample(pc_orig, labels_orig)
-        print("Voxelization", torch.bincount(labels)[255])
+        print("Voxelization", np.bincount(labels)[255])
 
         # Augment data
         if self.train_augmentations is not None:
@@ -151,7 +151,7 @@ class PCDataset(Dataset):
 
         # Crop to fov
         pc, labels = self.crop_to_fov(pc, labels)
-        print("Crop", torch.bincount(labels)[255])
+        print("Crop", np.bincount(labels)[255])
 
         # For each point, get index of corresponding 2D cells on projected grid
         cell_ind = self.get_occupied_2d_cells(pc)
