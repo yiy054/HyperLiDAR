6a5893bf798b1f8d90d016055995e927f507a873
Ivannia Gomez <82829719+DarthIV02@users.noreply.github.com>
2025-01-15 21:15:49 -0800

    Update scalr_and_hd.py

--- scalr_and_hd.py
+++ scalr_and_hd.py
@@ -106,7 +106,9 @@ class Feature_Extractor:
 
     def forward_model(self, it, batch):
         feat = batch["feat"]
+        print(feat.shape)
         labels = batch["labels_orig"]
+        print(labels.shape)
         cell_ind = batch["cell_ind"]
         occupied_cell = batch["occupied_cells"]
         neighbors_emb = batch["neighbors_emb"]
